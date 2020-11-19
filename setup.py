1af8c64494e6cc402a1ae35816808aea8cca624f
FR98 <ros18676@uvg.edu.gt>
2020-11-18 23:44:39 -0600

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-rest-framework-jet",
-    version="0.0.2",
+    version="0.0.3",
     author="Francisco Rosal",
     author_email="ros18676@uvg.edu.gt",
     description="JET - Django APP",
